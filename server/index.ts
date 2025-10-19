34525b2cacfa855d1914db54b4522d74b23089a8
tuhmaz <tuhmaz@gmail.com>
2025-10-19 12:59:46 +0200

    updata01

--- server/index.ts
+++ server/index.ts
@@ -54,8 +54,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: false, limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
 app.use((req, res, next) => {
   const start = Date.now();
